2fda368982fc4b9e8209a316845bda9e5af41917
Tom Lynch <tom@amristar.com>
2015-05-24 01:11:01 +1000

    remove generated

--- resources/public/js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [toptens.core]))
-(figwheel.client/start {:build-id "dev", :on-jsload toptens.core/on-js-reload, :websocket-url "ws://localhost:3450/figwheel-ws"})
-
